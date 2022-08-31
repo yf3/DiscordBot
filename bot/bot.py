9cf922bfd8abf678d3ac1a1485d3be3a5db3d3b8
Francis Yang <yangfrancis0331@gmail.com>
2022-09-01 02:41:03 +0800

    the kick and renames

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
-from discord import guild
+from discord import Member
+from discord.ext.commands import has_permissions, MissingPermissions
 from decouple import config
 
 intents = discord.Intents.all()
@@ -13,16 +14,27 @@ async def echo(ctx, message):
 
 @echo.error
 async def on_command_error(ctx, error):
-    if isinstance(error, commands.errors.MissingRequiredArgument):
+    if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send('No message to echo!')
     
 @bot.command()
-async def kick(ctx):
-    await ctx.send(content='kick?!')
+@has_permissions(kick_members=True)
+async def kick(ctx, member: discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+    await ctx.send(f'Bye {member} lol')
+
+@kick.error
+async def kick_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send(f'{ctx.author} has no permission to kick member!')
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('kick who?')
 
 @bot.command()
-async def newChannel(ctx, channel_name):
+async def newchannel(ctx, channel_name=None):
     current_guild = ctx.message.guild
+    if channel_name == None:
+        channel_name = "new-channel"
     await ctx.send(f'{channel_name} successfully created!')
     await current_guild.create_text_channel(channel_name)
 
