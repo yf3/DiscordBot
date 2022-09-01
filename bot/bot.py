1f6dba1547982a611f66b9a577965898e1d8f7cf
Francis Yang <yangfrancis0331@gmail.com>
2022-09-01 20:59:49 +0800

    exception handling in cog

--- bot/bot.py
+++ bot/bot.py
@@ -4,28 +4,27 @@ from discord.ext import commands, tasks
 from discord.ext.commands import has_permissions
 from decouple import config
 
-intents = discord.Intents.all()
-
 class MyBot(commands.Bot):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
     
     async def setup_hook(self) -> None:
-        self.predefined_task.start()
+        self.broadcasting_task.start()
         # self.loop.create_task(self.predefined_task()) # unable to find broadcast channel
 
     @tasks.loop(seconds=1.0, count=2)
-    async def predefined_task(self):
+    async def broadcasting_task(self):
         target_channel = discord.utils.get(self.get_all_channels(), guild__name='forTestingOnly', name='forbroadcast')
         if target_channel is None:
             print('Not found\n')
         else:
             await target_channel.send('broadcasting')
 
-    @predefined_task.before_loop
+    @broadcasting_task.before_loop
     async def before_my_task(self):
         await self.wait_until_ready()
 
+intents = discord.Intents.all()
 bot = MyBot(command_prefix='!', intents=intents)
 
 class Interactions(commands.Cog):
@@ -36,18 +35,33 @@ class Interactions(commands.Cog):
     async def echo(self, ctx, message_content):
         await ctx.send(f'{ctx.author}:{message_content}')
 
-@bot.command()
-@has_permissions(kick_members=True)
-async def kick(ctx, member: discord.Member, *, reason=None):
-    await member.kick(reason=reason)
-    await ctx.send(f'Bye {member} lol')
+    async def cog_command_error(self, ctx, error: Exception) -> None:
+        if isinstance(error, commands.MissingRequiredArgument):
+            correct_usage = f'{bot.command_prefix}{ctx.command.name} {ctx.command.signature}'
+            await ctx.send(f'{error} \ncorrect usage: {correct_usage}')
 
-@kick.error
-async def kick_error(ctx, error):
-    if isinstance(error, commands.MissingPermissions):
-        await ctx.send(f'{ctx.author} has no permission to kick member!')
-    elif isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send('Kick who?')
+class MemberManagement(commands.Cog):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+    
+    @commands.command()
+    @has_permissions(kick_members=True)
+    async def kick(self, ctx, member: discord.Member, *, reason=None):
+        await member.kick(reason=reason)
+        await ctx.send(f'Bye {member} lol')  
+
+# @bot.command()
+# @has_permissions(kick_members=True)
+# async def kick(ctx, member: discord.Member, *, reason=None):
+#     await member.kick(reason=reason)
+#     await ctx.send(f'Bye {member} lol')
+
+# @kick.error
+# async def kick_error(ctx, error):
+#     if isinstance(error, commands.MissingPermissions):
+#         await ctx.send(f'{ctx.author} has no permission to kick member!')
+#     elif isinstance(error, commands.MissingRequiredArgument):
+#         await ctx.send('Kick who?')
 
 @bot.command()
 async def newchannel(ctx, channel_name=None):
@@ -61,10 +75,10 @@ async def newchannel(ctx, channel_name=None):
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         await ctx.send(error)
-    await ctx.send(f'{error} \ncorrect usage: {bot.command_prefix}{ctx.command.name} {ctx.command.signature}')
 
 async def add_cogs():
     await bot.add_cog(Interactions(bot))
+    await bot.add_cog(MemberManagement(bot))
 
 async def main():
     await add_cogs()
